cc9de66136b8d73843c6cf3c02101e787201da67
Carleslc <lazaro.costa.carles@gmail.com>
2017-08-25 00:20:31 +0200

    Improved publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-set -e
-git checkout develop # force to be in develop branch
-git checkout examples
-git merge develop -m "Update from develop"
-git checkout gh-pages
-git pull origin gh-pages
-git merge develop --ff-only
-git checkout develop
-git push --all
+{
+    git pull --all
+    git checkout develop # force to be in develop branch
+    git checkout examples
+    git merge develop -m "Update from develop"
+    git checkout gh-pages
+    git pull origin gh-pages
+    git merge develop --ff-only
+    git checkout develop
+    git push --all
+} || {
+    git checkout develop
+}
